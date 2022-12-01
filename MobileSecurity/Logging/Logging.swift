8615a7b30018d2f70cf18967f0dc5b5aa1fb8739
vinayak-nickelfox <vinayak@nickelfox.com>
2022-12-01 17:10:25 +0530

    feat(logging): support printing log for debug env

--- MobileSecurity/Logging/Logging.swift
+++ MobileSecurity/Logging/Logging.swift
@@ -7,3 +7,20 @@
 
 import Foundation
 
+func print(_ items: Any..., separator: String = " ", terminator: String = "\n") {
+    
+#if DEBUG
+    var idx = items.startIndex
+    let endIdx = items.endIndex
+    repeat {
+        Swift.print(
+            items[idx],
+            separator: separator,
+            terminator: idx == (endIdx - 1) ? terminator : separator
+        )
+        
+        idx += 1
+    }
+    while idx < endIdx
+            #endif
+}
